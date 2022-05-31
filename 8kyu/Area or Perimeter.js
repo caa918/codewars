0700ddec8b3340d366f5b2099b063c4b24c159c3
Chad Adam <cadam@adamfamily.net>
2022-05-31 18:36:53 -0500

    Fixed typos
    Signed-off-by: Chad Adam <cadam@adamfamily.net>
    Signed-off-by: Chad Adam <30304547+caa918@users.noreply.github.com>

--- 8kyu/Area or Perimeter.js
+++ 8kyu/Area or Perimeter.js
@@ -1,5 +1,5 @@
 // Solution by Chad Adam
-// Kata: https://www.codewars.com/kata/5ab6538b379d20ad880000ab
+// Kata Link: https://www.codewars.com/kata/5ab6538b379d20ad880000ab
 
 const areaOrPerimeter = function(l , w) {
   return (l === w) ? (l * w) : ( l + l + w + w )
